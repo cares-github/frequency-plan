7b9ec32490ad4ada7b01c267a882d5a1a1bc92df
acw <aldcwatson@gmail.com>
2024-05-23 08:29:52 -0700

    Added type annotations to the CHIRP -> RT converter programme

--- bin/c2rt.py
+++ bin/c2rt.py
@@ -57,7 +57,7 @@ RT_to_CHIRP_map = {'Channel Number' : 'Location',
                    'Comment' : 'Comment'}
 
 # ----------------------------------------------------------------------------
-def import_CHIRP(input_file):
+def import_CHIRP(input_file: str) -> list[str]:
     raw_chirp_data = []
     with open(input_file, newline='') as csvfile:
         # The CSV file contains incident information. All we need
@@ -71,15 +71,16 @@ def import_CHIRP(input_file):
     return raw_chirp_data
 
 # ----------------------------------------------------------------------------
-def c2rt_channel_number(chirp_location):
+def c2rt_channel_number(chirp_location: str) -> int:
     return f"{int(chirp_location)}"
 
 # ----------------------------------------------------------------------------
-def c2rt_rx_frequency(chirp_frequency):
+def c2rt_rx_frequency(chirp_frequency: str) -> float:
     return f"{float(chirp_frequency):.5f}"
 
 # ----------------------------------------------------------------------------
-def c2rt_tx_frequency(rx_frequency, chirp_duplex, chirp_offset):
+def c2rt_tx_frequency(rx_frequency: float, chirp_duplex: str, 
+                      chirp_offset: str) -> tuple[str, str]:
     # Convert the string offset freq to a float because we're going to
     # do some maths.
     offset = float(chirp_offset)
@@ -98,7 +99,7 @@ def c2rt_tx_frequency(rx_frequency, chirp_duplex, chirp_offset):
     return (tx_freq_str, offset_direction)
 
 # ----------------------------------------------------------------------------
-def c2rt_offset(chirp_offset):
+def c2rt_offset(chirp_offset: str) -> str:
     # All CHIRP frequencies are in megahertz. Need to do some
     # conversion. RT is a bit awkward here.
     offset = float(chirp_offset)
@@ -115,8 +116,8 @@ def c2rt_offset(chirp_offset):
     return rt_offset_frequency
 
 # ----------------------------------------------------------------------------
-def c2rt_tone(chirp_tone):
-    rt_tone_mode = 'None'
+def c2rt_tone(chirp_tone: str) -> str:
+    rt_tone_mode: str = 'None'
     if chirp_tone == 'Tone':
         rt_tone_mode = 'Tone'
     elif chirp_tone == 'TSQL':
@@ -126,27 +127,27 @@ def c2rt_tone(chirp_tone):
     return rt_tone_mode
 
 # ----------------------------------------------------------------------------
-def c2rt_CTCSS_tone(chirp_tone_mode, chirp_rx_tone_freq):
-    rt_tone_mode = c2rt_tone(chirp_tone_mode)
-    tone_freq = float(chirp_rx_tone_freq)
+def c2rt_CTCSS_tone(chirp_tone_mode: str, chirp_rx_tone_freq: str) -> str:
+    rt_tone_mode: str = c2rt_tone(chirp_tone_mode)
+    tone_freq: float = float(chirp_rx_tone_freq)
     rt_CTCSS_tone = ''
     if rt_tone_mode == 'Tone' or rt_tone_mode == 'T Sql':
         rt_CTCSS_tone = f"{tone_freq:.1f} Hz"
     return rt_CTCSS_tone
 
 # ----------------------------------------------------------------------------
-def c2rt_DCS(chirp_dtcs_code):
-    rt_dcs = ''
+def c2rt_DCS(chirp_dtcs_code: str) -> str:
+    rt_dcs: str = ''
     if chirp_dtcs_code == 'DTCS':
         rt_dcs = f"DCS"
     return rt_dcs
 
 # ----------------------------------------------------------------------------
-def c2rt_step(chirp_step):
+def c2rt_step(chirp_step: str) -> str:
     return f"{chirp_step} kHz"
 
 # ----------------------------------------------------------------------------
-def truncate_comments(chirp_comment, max_size):
+def truncate_comments(chirp_comment: str, max_size: int) -> str:
     if max_size < 0 or max_size > 60:
         raise ValueError("Comment maximum size must be between "
                          f"0 and 60 inclusive. Given {max_size}")
@@ -156,18 +157,18 @@ def truncate_comments(chirp_comment, max_size):
     return chirp_comment[:max_size - 3] + "..."
 
 # ----------------------------------------------------------------------------
-def chirp2rt(raw_chirp_data):
-    rt_data = []
+def chirp2rt(raw_chirp_data: list[str]) -> list[dict[str : str]]:
+    rt_data: list[dict[str: str]] = []
     # Map the chirp data to RT columns. Clean up any formatting.
-    channel_number = 1
+
     for chirp_row in raw_chirp_data:
-        rt_row = {}
+        rt_row: dict[str : str] = {}
 
         # RT uses Channel Number whereas CHIRP uses Location
         rt_row['Channel Number'] = c2rt_channel_number(chirp_row['Location'])
 
         # RT Receive Frequency
-        receive_frequency = c2rt_rx_frequency(chirp_row['Frequency'])
+        receive_frequency: float = c2rt_rx_frequency(chirp_row['Frequency'])
         rt_row['Receive Frequency'] = receive_frequency
 
         # RT Transmit Frequency and Offset Direction
@@ -216,24 +217,25 @@ def chirp2rt(raw_chirp_data):
 
         # Add the new RT row into the list
         rt_data.append(rt_row)
-        channel_number += 1
     return rt_data
 
 # ----------------------------------------------------------------------------
-def export_RT(output_file, RT_rows):
+def export_RT(output_file: str, RT_rows: list[dict[str:str]]) -> int:
 
     with open(output_file, 'w', newline='') as csvfile:
         rt_writer = csv.DictWriter(csvfile, fieldnames=RT_headers)
         rt_writer.writeheader()
+        num_rows: int = 1 # including the header
         for row in RT_rows:
             rt_writer.writerow(row)
-
+            num_rows += 1
+        return num_rows
 # ----------------------------------------------------------------------------
-def convert(input_file, output_file):
+def convert(input_file: str, output_file: str) -> int:
     raw_chirp_content = import_CHIRP(input_file)
     RT_content = chirp2rt(raw_chirp_content)
-    export_RT(output_file, RT_content)
-
+    num_rows_written: int = export_RT(output_file, RT_content)
+    return num_rows_written
 # ----------------------------------------------------------------------------
 # Programme entry point
 # ----------------------------------------------------------------------------
@@ -249,4 +251,5 @@ if __name__ == '__main__':
 #   output_file = '../rt_csv/RT_GMRS.csv'
 #    input_file = '../chirp_csv/HF.csv'
 #    output_file = '../rt_csv/RT_HF.csv'
-    convert(input_file, output_file)
+    num_rows: int = convert(input_file, output_file)
+    print(f'{num_rows} channels converted from CHIRP format to RT Systems format.')
